59014ce082ea637006759a3192d48877bd293b6f
Yaros <Yaroslavhik@yandex.ua>
2021-11-15 14:25:17 +0100

    added validation for name

--- src/components/ModalValidate.vue
+++ src/components/ModalValidate.vue
@@ -1,14 +1,25 @@
 <template>
   <Modal
-      title="Modal with form"
+      title="Modal with form + Validate"
       @close="$emit('close')"
   >
+<!--    body-->
     <div slot="body">
       <form @submit.prevent="">
-        <label>Name:</label>
-        <input type="text">
+
+<!--        name-->
+        <div class="form-item" :class="{ errorInput: $v.name.$error }">
+          <label>Name:</label>
+          <p class="errorText" v-if="!$v.name.required"> Field is required! </p>
+          <p class="errorText" v-if="!$v.name.minLength"> Name must have at least {{ $v.name.$params.minLength.min }} !</p>
+          <input
+              v-model="name"
+              :class="{ error: $v.name.$error }"
+              @change="$v.name.$touch()"
+          >
+        </div>
         <label>Email:</label>
-        <input type="email">
+        <input v-bind="email">
         <button class="btn btnPrimary">Submit</button>
       </form>
     </div>
@@ -17,12 +28,45 @@
 
 <script>
 
-import { required, minLength, email } from 'vuelidate/lib/validators'
+import {required, minLength, email} from 'vuelidate/lib/validators';
 
 import Modal from "./UI/Modal";
 
 export default {
   name: "ModalValidate",
-  components: {Modal}
+  components: {Modal},
+  data() {
+    return {
+      name: '',
+      email: ''
+    }
+  },
+  validations: {
+    name: {
+      required,
+      minLength: minLength(4)
+    },
+    email: {
+      required,
+      email
+    }
+  },
 }
 </script>
+
+<style lang="scss">
+.form-item .errorText {
+  display: none;
+  margin-bottom: 8px;
+  font-size: 13.4px;
+  color: #de4040;
+}
+.form-item {
+  &.errorInput .errorText {
+    display: block;
+  }
+}
+input.error {
+  border-color: #de4040;
+}
+</style>
